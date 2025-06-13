0e2d9a2ca63e669fe06a05069536fa33f2f80a0a
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-06-13 16:51:57 +0900

    テストを書いて

--- app/api/todo-items/route.test.ts
+++ app/api/todo-items/route.test.ts
@@ -1,13 +1,16 @@
-import { dbFetchAllTodoItems } from '@/lib/db/todo-items';
+import { dbCreateTodoItem, dbFetchAllTodoItems } from '@/lib/db/todo-items';
 import { todo_item } from '@prisma/client';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
-import { GET } from './route';
+import { GET, POST } from './route';
+import { NextRequest } from 'next/server';
 
 vi.mock('@/lib/db/todo-items', () => ({
     dbFetchAllTodoItems: vi.fn(),
+    dbCreateTodoItem: vi.fn(),
 }));
 
 const mockDbFetchAllTodoItems = vi.mocked(dbFetchAllTodoItems);
+const mockDbCreateTodoItem = vi.mocked(dbCreateTodoItem);
 
 describe('GET /api/todo-items', () => {
     describe('success case', () => {
@@ -57,6 +60,73 @@ describe('GET /api/todo-items', () => {
             expect(response.status).toBe(500);
             expect(body).toEqual({ error: "Failed to fetch todo items" });
             expect(mockDbFetchAllTodoItems).toHaveBeenCalledTimes(1);
+        });
+    });
+});
+
+describe('POST /api/todo-items', () => {
+    describe('success case', () => {
+        beforeEach(() => {
+            vi.clearAllMocks();
+        });
+
+        it('creates a new todo item and responds with it', async () => {
+            const mockTodoItem: todo_item = {
+                id: '1',
+                title: 'New Todo',
+                description: 'New Description',
+                deadline: null,
+                completed: false,
+            };
+            mockDbCreateTodoItem.mockResolvedValue(mockTodoItem);
+
+            const request = new NextRequest('http://localhost/api/todo-items', {
+                method: 'POST',
+                body: JSON.stringify({
+                    title: 'New Todo',
+                    description: 'New Description',
+                    deadline: null,
+                    completed: false,
+                }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+
+            const response = await POST(request);
+            const body = await response.json();
+
+            expect(response.status).toBe(200);
+            expect(body).toEqual(mockTodoItem);
+            expect(mockDbCreateTodoItem).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe('failure case', () => {
+        const consoleErrorSpy = vi.spyOn(console, 'error').mockImplementation(() => { });
+        beforeEach(() => {
+            vi.clearAllMocks();
+            consoleErrorSpy.mockClear();
+        });
+
+        it('responds with 500 on error', async () => {
+            mockDbCreateTodoItem.mockRejectedValue(new Error("Database error"));
+
+            const request = new NextRequest('http://localhost/api/todo-items', {
+                method: 'POST',
+                body: JSON.stringify({
+                    title: 'New Todo',
+                    description: 'New Description',
+                    deadline: null,
+                    completed: false,
+                }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+
+            const response = await POST(request);
+            const body = await response.json();
+
+            expect(response.status).toBe(500);
+            expect(body).toEqual({ error: "Failed to create todo item" });
+            expect(mockDbCreateTodoItem).toHaveBeenCalledTimes(1);
         });
     });
 });
