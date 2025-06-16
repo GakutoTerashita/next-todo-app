28010e4ccd29cc5da4420f8139f4db0f21d57628
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-06-16 14:56:53 +0900

    よし

--- components/ItemRegistrationForm.tsx
+++ components/ItemRegistrationForm.tsx
@@ -1,46 +1,25 @@
 'use client';
 
 import useTodoListItemRegistration from "@/hooks/useTodoListItemRegistration";
+import useTodoListItemRegistrationForm from "@/hooks/useTodoListItemRegistrationForm";
 import { Button, TextField } from "@mui/material";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { todo_item } from "@prisma/client";
-import { Dayjs } from "dayjs";
-import React, { useState } from "react";
-
-const handleSubmit = async (
-    e: React.FormEvent,
-    title: string,
-    description: string,
-    deadline: Dayjs | null,
-    register: (title: string, description: string, deadline: Dayjs | null) => Promise<todo_item>,
-) => {
-    e.preventDefault();
-
-    if (!title) {
-        alert("Please fill in the title");
-        return;
-    }
-
-    try {
-        const result = await register(title, description, deadline);
-        console.log("Item registered successfully:", result);
-    } catch (error) {
-        console.error("Failed to register item:", error);
-    }
-};
-
+import React from "react";
 
 const ItemRegistrationForm = () => {
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [deadline, setDeadline] = useState<Dayjs | null>(null);
+    const {
+        title,
+        description,
+        deadline,
+        handleSubmit,
+        onRegistered,
+        syncTitleInputField,
+        syncDeadlineInputField,
+        syncDescriptionInputField,
+    } = useTodoListItemRegistrationForm();
 
-    const { loading, register, } = useTodoListItemRegistration(() => {
-        setTitle("");
-        setDescription("");
-        setDeadline(null);
-    });
+    const { loading, register, } = useTodoListItemRegistration(onRegistered);
 
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -49,30 +28,24 @@ const ItemRegistrationForm = () => {
                 name="title"
                 placeholder="Enter item name"
                 value={title}
-                onChange={(e) => setTitle(e.target.value)}
+                onChange={syncTitleInputField}
             />
             <TextField
                 type="text"
                 name="description"
                 placeholder="Enter item description"
                 value={description}
-                onChange={(e) => setDescription(e.target.value)}
+                onChange={syncDescriptionInputField}
             />
             <DatePicker
                 value={deadline}
-                onChange={(newValue) => setDeadline(newValue)}
+                onChange={syncDeadlineInputField}
             />
             <Button
                 type="submit"
                 loading={loading}
-                onClick={
-                    (e) => handleSubmit(
-                        e,
-                        title,
-                        description,
-                        deadline,
-                        register,
-                    )}>
+                onClick={(e) => handleSubmit(e, register)}
+            >
                 Add Item
             </Button>
         </LocalizationProvider>
