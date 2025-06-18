baa7a1c1880380860bae13cedb7c0d370df07b20
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-06-18 10:24:43 +0900

    はぁ

---
+++ no-suggested-imports-for-react-testing-library-in-vs-code.d.ts
@@ -0,0 +1,3 @@
+// Workaround for VS Code not suggesting imports for React Testing Library
+// https://stackoverflow.com/questions/76652592/no-suggested-imports-for-react-testing-library-in-vs-code
+/// <reference types="@testing-library/react" />
