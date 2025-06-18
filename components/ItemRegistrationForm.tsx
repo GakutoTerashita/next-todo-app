a0f14e9ffe1ff7485cc18858c3d5abaaabeef854
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-06-18 10:01:45 +0900

    掃除

--- components/ItemRegistrationForm.tsx
+++ components/ItemRegistrationForm.tsx
@@ -1,24 +1,39 @@
 'use client';
 
-import useTodoListItemRegistration from "@/hooks/useAccountRegistration";
 import { Button, TextField } from "@mui/material";
 import React from "react";
 
 const ItemRegistrationForm = () => {
-    const {
-        loading,
-        register,
-        setTitle,
-        setDescription,
-        setDeadline,
-    } = useTodoListItemRegistration();
+    const [title, setTitle] = React.useState("");
+    const [description, setDescription] = React.useState("");
+    const [deadline, setDeadline] = React.useState<Date | null>(null);
 
     return (
         <React.Fragment>
-            <TextField type="text" name="title" placeholder="Enter item name" onChange={(e) => setTitle(e.target.value)} />
-            <TextField type="text" name="description" placeholder="Enter item description" onChange={(e) => setDescription(e.target.value)} />
-            <TextField type="datetime-local" name="deadline" onChange={(e) => setDeadline(new Date(e.target.value))} />
-            <Button type="submit" loading={loading} onClick={register}>Add Item</Button>
+            <form onSubmit={async (e) => {
+                e.preventDefault();
+                // TODO
+            }}>
+                <TextField
+                    type="text"
+                    name="title"
+                    placeholder="Enter item name"
+                    onChange={(e) => setTitle(e.target.value)} />
+                <TextField
+                    type="text"
+                    name="description"
+                    placeholder="Enter item description"
+                    onChange={(e) => setDescription(e.target.value)} />
+                <TextField
+                    type="datetime-local"
+                    name="deadline"
+                    onChange={(e) => setDeadline(new Date(e.target.value))} />
+                <Button
+                    type="submit"
+                >
+                    Add Item
+                </Button>
+            </form>
         </React.Fragment>
     );
 }
