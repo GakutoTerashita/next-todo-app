b2ab33b857c72c4c044a44b841f5898faed2afb7
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-06-18 09:56:54 +0900

    Revert "推論してくれるからいらないか"
    This reverts commit a163d4be266a714e0dbb7ee3578eb13cee56a19c.

--- hooks/useTodoListItemRegistration.ts
+++ hooks/useTodoListItemRegistration.ts
@@ -1,6 +1,7 @@
 'use client';
 
 import { registerTodoItem } from "@/lib/api/todo-items";
+import { todo_item } from "@prisma/client";
 import useFetch from "./useFetch";
 
 const useTodoListItemRegistration = (
@@ -11,7 +12,7 @@ const useTodoListItemRegistration = (
         loading,
         error,
         executeFetch,
-    } = useFetch(
+    } = useFetch<todo_item, Omit<todo_item, "id" | "completed">>(
         registerTodoItem,
         (error) => {
             console.error("Error registering todo item:", error);
