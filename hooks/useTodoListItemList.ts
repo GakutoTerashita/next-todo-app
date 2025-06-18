229ea8e7c3bcb2d0a5a1f274f17d701d48d9f2c9
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-06-18 09:56:57 +0900

    Revert "すげー"
    This reverts commit 26d43cedf019e02449ee9946c8f506fff56dc3fb.

--- hooks/useTodoListItemList.ts
+++ hooks/useTodoListItemList.ts
@@ -1,19 +1,33 @@
 'use client';
 
 import { fetchTodoItems } from "@/lib/api/todo-items";
-import useAsync from "./useAsync";
+import { todo_item } from "@prisma/client";
+import { useEffect, useState } from "react";
 
 const useTodoListItemList = () => {
-    const {
-        data: fetchedTodoItems,
-        loading,
-        error,
-    } = useAsync(fetchTodoItems);
+    const [todoListItems, setTodoListItems] = useState<todo_item[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const fetchedItems = await fetchTodoItems();
+                setTodoListItems(fetchedItems);
+            } catch (error) {
+                console.error("Failed to fetch todo items:", error);
+                setTodoListItems([]);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return {
-        todoListItems: fetchedTodoItems || [],
+        todoListItems,
         loading,
-        error,
     };
 };
 
