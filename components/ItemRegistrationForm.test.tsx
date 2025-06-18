39b99ff64292698aa5b5d66edfb19ba78bc4be62
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-06-18 15:02:10 +0900

    ok

--- components/ItemRegistrationForm.test.tsx
+++ components/ItemRegistrationForm.test.tsx
@@ -1,6 +1,7 @@
 import { cleanup, render } from "@testing-library/react";
 import { afterEach, describe, expect, it } from "vitest";
 import ItemRegistrationForm from "./ItemRegistrationForm";
+import userEvent from "@testing-library/user-event";
 
 describe('ItemRegistrationForm', () => {
     afterEach(() => {
@@ -19,5 +20,22 @@ describe('ItemRegistrationForm', () => {
 
         const datePicker = result.getByLabelText('deadline');
         expect(datePicker.getAttribute('type')).toBe('date');
+    });
+
+    it('user input is reflected in the input fields', async () => {
+        const user = userEvent.setup();
+        const result = render(<ItemRegistrationForm />);
+
+        const titleInput = result.getByRole('textbox', { name: 'title' }) as HTMLInputElement;
+        const descriptionInput = result.getByRole('textbox', { name: 'description' }) as HTMLInputElement;
+        const datePicker = result.getByLabelText('deadline') as HTMLInputElement;
+
+        await user.type(titleInput, 'Test Item');
+        await user.type(descriptionInput, 'This is a test item description.');
+        await user.type(datePicker, '2023-10-01');
+
+        expect(titleInput.value).toBe('Test Item');
+        expect(descriptionInput.value).toBe('This is a test item description.');
+        expect(datePicker.value).toBe('2023-10-01');
     });
 });
