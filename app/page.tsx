2ef3c27cc0df264efd1ff9da08d36b45700dfa62
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-07-01 16:00:27 +0900

    見づらい...

--- app/page.tsx
+++ app/page.tsx
@@ -13,20 +13,26 @@ const Home = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
       <HeaderBar />
-      <Container maxWidth="lg" sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        flexGrow: 1,
-        paddingY: 2
-      }}>
-        <Paper elevation={3} sx={{
-          padding: 1,
-          flexGrow: 1,
+      <Container
+        maxWidth="lg"
+        sx={{
           display: 'flex',
           flexDirection: 'column',
-          overflow: 'hidden',
-          maxHeight: 'calc(100vh - 212px)' // Adjust based on header and form below
-        }}>
+          flexGrow: 1,
+          paddingY: 2
+        }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            padding: 1,
+            flexGrow: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            overflow: 'hidden',
+            maxHeight: 'calc(100vh - 212px)' // Adjust based on header and form below
+          }}
+        >
           <div style={{ overflowY: 'auto', flexGrow: 1 }}>
             {query.isLoading && (
               <List>
