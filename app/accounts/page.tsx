2382dd52837bb1d4f2aa2a90a267e53f55bc68ff
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-07-03 15:06:28 +0900

    layout

--- app/accounts/page.tsx
+++ app/accounts/page.tsx
@@ -1,13 +1,15 @@
 import LoginForm from "@/components/LoginForm";
 import SignupForm from "@/components/SignupForm"
+import { Container, Divider } from "@mui/material";
 import React from "react";
 
 const AccountsPage = () => {
     return (
-        <React.Fragment>
+        <Container maxWidth="lg">
             <SignupForm />
+            <Divider sx={{ marginY: 2 }} />
             <LoginForm />
-        </React.Fragment>
+        </Container>
     );
 }
 
