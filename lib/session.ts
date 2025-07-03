a01ad6fe3537a85f5688f5d14533579ebb04cf0f
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-07-03 13:44:57 +0900

    jwt

---
+++ lib/session.ts
@@ -0,0 +1,44 @@
+import "server-only";
+import { JWTPayload, SignJWT, jwtVerify } from "jose";
+import { cookies } from "next/headers";
+
+const secretKey = process.env.SESSION_SECRET;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+export const encrypt = async (payload: JWTPayload) => {
+    const jwt = await new SignJWT(payload)
+        .setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .setExpirationTime("7d")
+        .sign(encodedKey);
+    return jwt;
+};
+
+export const decrypt = async (session: string | undefined = "") => {
+    try {
+        const { payload } = await jwtVerify(
+            session,
+            encodedKey,
+            {
+                algorithms: ["HS256"],
+            }
+        )
+        return payload;
+    } catch (error) {
+        console.error("Failed to decrypt session:", error);
+    }
+};
+
+export const createSession = async (userId: string) => {
+    const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+    const session = await encrypt({ userId, expiresAt });
+    const cookieStore = await cookies();
+
+    cookieStore.set("session", session, {
+        httpOnly: true,
+        secure: true,
+        expires: expiresAt,
+        sameSite: "lax",
+        path: "/",
+    });
+};
